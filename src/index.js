bc50ece0a90674085ce28a5a987f12e17ee94f6b
halilesmer <taximinator@gmail.com>
2021-12-18 19:24:23 +0100

    you can add items

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { render } from "react-dom";
 import reportWebVitals from './reportWebVitals';
 import StoreFront from "./Components/StoreFront.js";
 import "./index.css";
 
+
+
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  if (loggedIn) {
-    return (
-      <>
-        <StoreFront />
-        <button className="btn btn-outline" onClick={() => setLoggedIn(false)}>
-          Logout
-        </button>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h2>Please login</h2>
-        <button className="btn btn-primary" onClick={() => setLoggedIn(true)}>
-          Login
-        </button>
-        {/* <StoreFront /> */}
-      </>
-    );
-  }
+  return <StoreFront />;
 }
+
 
 render(<App />, document.querySelector("#react-root"));
 
