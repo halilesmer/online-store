fe3c227e68517cf7a20488d0e1d160a0fd24d524
halilesmer <taximinator@gmail.com>
2021-12-25 11:06:10 +0100

    Post Online Store Admin

--- src/Components/StoreFront.js
+++ src/Components/StoreFront.js
@@ -3,7 +3,6 @@ import ProductsList from "./ProductsList.js";
 import AddProductForm from "./AddProductForm.js";
 
 export default function StoreFront() {
-/* const [products, setProducts] = useState([]); */
 const [products, setProducts] = useState(() => {
     const savedProducts = localStorage.getItem("products");
     if (savedProducts) {
@@ -17,12 +16,13 @@ const [products, setProducts] = useState(() => {
   const [description, setDescription] = useState("");
   const [validation, setValidation] = useState("");
 
+  /* save items to local storage */
   useEffect(() => {
     localStorage.setItem("products", JSON.stringify(products));
     console.log("local", products);
   }, [products]);
 
-  function handleFormSubmit(event) {
+  async function handleFormSubmit(event) {
     event.preventDefault();
 
     if (!name) {
@@ -33,18 +33,35 @@ const [products, setProducts] = useState(() => {
       setValidation("Please enter a description");
       return;
     }
-    setProducts([
-      ...products,
-      {
-        id: products.length + 1,
-        name: name,
-        description,
-      },
-    ]);
-    setName("");
-    setDescription("");
-    setValidation("");
-  }
+
+ try{
+      const response = await fetch('https://api.learnjavascript.online/demo/react/admin/products', {
+            method: 'post',
+           headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+            name: name,
+            description, 
+            })
+        })
+        const data = await response.json();
+            console.log(data) 
+        if(data){
+            setProducts([...products, {
+            id: products.length + 1,
+            name: name,
+            description,
+        }]);
+        setName("");
+        setDescription("");
+        setValidation("");
+        }   
+        }catch{console.log('error')}
+
+    }
+
+  
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -68,7 +85,9 @@ const [products, setProducts] = useState(() => {
         onDescriptionChange={handleDescriptionChange}
         onFormSubmit={handleFormSubmit}
       />
+      
       <div>{products.length === 0 && <p>Add your first product</p>}</div>
+
       <ProductsList products={products} onDeleteClick={handleDeleteClick} />
     </>
   );
